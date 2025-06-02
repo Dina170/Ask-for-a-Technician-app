b8c716869b87847ad9cbf2a384950a427e20cc52
dina <dinaelhadidy966@gmail.com>
2025-06-02 22:54:38 +0300

    feat: add mailer utility for sending password reset emails

---
+++ utils/mailer.js
@@ -0,0 +1,19 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+exports.sendResetEmail = async (to, token) => {
+  const resetURL = `http://localhost:3000/reset-password/${token}`;
+  await transporter.sendMail({
+    to,
+    from: process.env.EMAIL,
+    subject: "Password Reset",
+    html: `<p>Click here to reset your password: <a href="${resetURL}">${resetURL}</a></p>`,
+  });
+};
