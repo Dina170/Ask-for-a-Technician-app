2153d9b9a221fc91d29a0e4d3b2f0936deada3c9
Hagar Elmadany <hagarelmadany@gmail.com>
2025-06-04 05:27:34 +0300

    add route to technicians page

--- routes/public/technician.route.js
+++ routes/public/technician.route.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const techController = require('../../controllers/public/technician.controller');
 
 
+// Show all technicians page
+router.get('/', techController.getAllTechnicians);
+
 // New route for "See More" page with search
 router.get('/:id/seeMoreTechnicianNeighborhoods', techController.getSeeMoreTechnicianNeighborhoods);
 // Show technician neighborhoods list
@@ -10,6 +13,7 @@ router.get('/:id/neighborhoods', techController.getTechnicianNeighborhoods);
 
 // Show specific neighborhood details for a technician
 router.get('/:techId/neighborhoods/:neighId', techController.getNeighborhoodDetails);
+
 
 
 module.exports = router;
