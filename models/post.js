18d26358358e29c6eeaa72c77458193a991d28fa
dina <dinaelhadidy966@gmail.com>
2025-10-06 21:07:45 +0300

    Change posts slug to name

--- models/post.js
+++ models/post.js
@@ -3,14 +3,15 @@ const { Schema, model } = require("mongoose");
 const PostSchema = new Schema({
   blog: { type: Schema.Types.ObjectId, ref: "Blog", required: true },
   name: { type: String, required: true },
+  slug: { type: String, unique: true },
   permaLink: { type: String, required: true, unique: true },
   title: { type: String, required: true },
   content: { type: String, required: true },
 });
 
 PostSchema.pre("save", function (next) {
-  if (this.isModified("permaLink")) {
-    this.permaLink = this.permaLink.trim().replace(/\s+/g, "-");
+  if (this.isModified("name")) {
+    this.slug = this.name.trim().replace(/\s+/g, "-");
   }
 
   next();
