94fedd38f10d8173f123e122372d629f3532f07a
dina <dinaelhadidy966@gmail.com>
2025-10-06 20:17:37 +0300

    Remove details from route

--- routes/public/technician.route.js
+++ routes/public/technician.route.js
@@ -8,8 +8,7 @@ router.use((req, res, next) => {
 });
 
 router.get("/", techController.getAllTechnicians);
-router.get("/:slug/details", techController.getTechnicianDetails);
 router.get("/:slug", techController.getTechnicianDetails);
-router.get("/:slug/:section", techController.getSeeMoreTechnicianNeighborhoods);
+router.get("/:section/:slug", techController.getSeeMoreTechnicianNeighborhoods);
 
 module.exports = router;
